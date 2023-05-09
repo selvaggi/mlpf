3f6f342f7aa14659a5e116d64c4aa43761946a13
Michele Selvaggi <michele.selvaggi@uclouvain.be>
2023-05-09 14:53:51 +0200

    make variable length gen links

--- scripts/make_pftree.py
+++ scripts/make_pftree.py
@@ -1,13 +1,14 @@
 import sys
-from array import array
 import math
+import ROOT
+from array import array
 from ROOT import TFile, TTree
 
 debug = False
 
 """
 source /cvmfs/fcc.cern.ch/sw/latest/setup.sh
-python make_pftree.py /afs/cern.ch/user/b/brfranco/work/public/Fellow/FCCSW/221123/LAr_scripts/FCCSW_ecal/output_fullCalo_SimAndDigi_withCluster_MagneticField_False_pMin_5000_MeV_ThetaMinMax_50_130_pdgId_211_pythiaFalse_NoiseFalse.root tree.root
+python scripts/make_pftree.py /afs/cern.ch/user/b/brfranco/work/public/Fellow/FCCSW/221123/LAr_scripts/FCCSW_ecal/output_fullCalo_SimAndDigi_withCluster_MagneticField_False_pMin_5000_MeV_ThetaMinMax_50_130_pdgId_211_pythiaFalse_NoiseFalse.root tree.root
 """
 
 # source /cvmfs/fcc.cern.ch/sw/latest/setup.sh
@@ -31,70 +32,81 @@ numberOfEntries = ev.GetEntries()
 out_root = TFile(output_file, "RECREATE")
 t = TTree("events", "pf tree lar")
 
+vector_float = ROOT.std.vector("float")()
+vector_vector_float = ROOT.std.vector(ROOT.std.vector("float"))()
+
 event_number = array("i", [0])
 n_hit = array("i", [0])
 n_part = array("i", [0])
 
-max_hit = 10000
-max_part = 1000
-
-hit_x = array("f", max_hit * [0.0])
-hit_y = array("f", max_hit * [0.0])
-hit_z = array("f", max_hit * [0.0])
-hit_t = array("f", max_hit * [0.0])
-hit_p = array("f", max_hit * [0.0])
-hit_e = array("f", max_hit * [0.0])
-hit_theta = array("f", max_hit * [0.0])
-hit_phi = array("f", max_hit * [0.0])
+hit_x = ROOT.std.vector("float")()
+hit_y = ROOT.std.vector("float")()
+hit_z = ROOT.std.vector("float")()
+hit_t = ROOT.std.vector("float")()
+hit_p = ROOT.std.vector("float")()
+hit_e = ROOT.std.vector("float")()
+hit_theta = ROOT.std.vector("float")()
+hit_phi = ROOT.std.vector("float")()
 
 ### store here whether track: 0 /ecal: 1/hcal: 2
-hit_type = array("f", max_hit * [0.0])
+hit_type = ROOT.std.vector("int")()
 
-### store here the position of the corresponding gen particle associated to the hit
-hit_link1 = array("f", max_hit * [0.0])
-hit_link2 = array("f", max_hit * [0.0])
-hit_link3 = array("f", max_hit * [0.0])
-hit_link4 = array("f", max_hit * [0.0])
-hit_link5 = array("f", max_hit * [0.0])
-
+### store here the position of the corresponding gen particles associated to the hit
+hit_genlink = ROOT.std.vector(ROOT.std.vector("int"))()
 
 ## store here true information
-part_p = array("f", max_part * [0.0])
-part_e = array("f", max_part * [0.0])
-part_theta = array("f", max_part * [0.0])
-part_phi = array("f", max_part * [0.0])
-part_m = array("f", max_part * [0.0])
-part_pid = array("f", max_part * [0.0])
+part_p = ROOT.std.vector("float")()
+part_e = ROOT.std.vector("float")()
+part_theta = ROOT.std.vector("float")()
+part_phi = ROOT.std.vector("float")()
+part_m = ROOT.std.vector("float")()
+part_pid = ROOT.std.vector("float")()
 
 
 t.Branch("event_number", event_number, "event_number/I")
 t.Branch("n_hit", n_hit, "n_hit/I")
 t.Branch("n_part", n_part, "n_part/I")
 
-t.Branch("hit_x", hit_x, "hit_x[n_hit]/F")
-t.Branch("hit_y", hit_y, "hit_y[n_hit]/F")
-t.Branch("hit_z", hit_z, "hit_z[n_hit]/F")
-t.Branch("hit_t", hit_t, "hit_t[n_hit]/F")
-t.Branch("hit_p", hit_p, "hit_p[n_hit]/F")
-t.Branch("hit_e", hit_e, "hit_e[n_hit]/F")
-t.Branch("hit_theta", hit_theta, "hit_theta[n_hit]/F")
-t.Branch("hit_phi", hit_phi, "hit_phi[n_hit]/F")
-t.Branch("hit_type", hit_type, "hit_type[n_hit]/F")
-t.Branch("hit_link1", hit_link1, "hit_link1[n_hit]/F")
-t.Branch("hit_link2", hit_link2, "hit_link2[n_hit]/F")
-t.Branch("hit_link3", hit_link3, "hit_link3[n_hit]/F")
-t.Branch("hit_link4", hit_link4, "hit_link4[n_hit]/F")
-t.Branch("hit_link5", hit_link5, "hit_link5[n_hit]/F")
+t.Branch("hit_x", hit_x)
+t.Branch("hit_y", hit_y)
+t.Branch("hit_z", hit_z)
+t.Branch("hit_t", hit_t)
+t.Branch("hit_p", hit_p)
+t.Branch("hit_e", hit_e)
+t.Branch("hit_theta", hit_theta)
+t.Branch("hit_phi", hit_phi)
+t.Branch("hit_type", hit_type)
 
-t.Branch("part_p", part_p, "part_p[n_part]/F")
-t.Branch("part_theta", part_theta, "part_theta[n_part]/F")
-t.Branch("part_phi", part_phi, "part_phi[n_part]/F")
-t.Branch("part_m", part_m, "part_m[n_part]/F")
-t.Branch("part_pid", part_pid, "part_pid[n_part]/F")
+# Create a branch for the hit_genlink_flat
+t.Branch("hit_genlink", hit_genlink)
+
+t.Branch("part_p", part_p)
+t.Branch("part_theta", part_theta)
+t.Branch("part_phi", part_phi)
+t.Branch("part_m", part_m)
+t.Branch("part_pid", part_pid)
 
 for i, e in enumerate(ev):
 
     event_number[0] = i
+
+    # clear all the vectors
+    hit_x.clear()
+    hit_y.clear()
+    hit_z.clear()
+    hit_t.clear()
+    hit_p.clear()
+    hit_e.clear()
+    hit_theta.clear()
+    hit_phi.clear()
+    hit_type.clear()
+    hit_genlink.clear()
+    part_p.clear()
+    part_e.clear()
+    part_theta.clear()
+    part_phi.clear()
+    part_m.clear()
+    part_pid.clear()
 
     if (i + 1) % 1000 == 0:
         print(" ... processed {} events ...".format(i + 1))
@@ -109,92 +121,82 @@ for i, e in enumerate(ev):
     """
 
     for j, part in enumerate(ev.genParticles):
-        part_p[j] = math.sqrt(part.momentum.x ** 2 + part.momentum.y ** 2 + part.momentum.z ** 2)
-        part_theta[j] = math.acos(part.momentum.z / part_p[j])
-        part_phi[j] = math.atan2(part.momentum.y, part.momentum.x)
-        part_m[j] = part.mass
-        part_pid[j] = part.PDG
+        part_p.push_back(math.sqrt(part.momentum.x ** 2 + part.momentum.y ** 2 + part.momentum.z ** 2))
+        part_theta.push_back(math.acos(part.momentum.z / part_p[j]))
+        part_phi.push_back(math.atan2(part.momentum.y, part.momentum.x))
+        part_m.push_back(part.mass)
+        part_pid.push_back(part.PDG)
 
     ## TODO: WHEN TRACKS WILL BE AVAILABLES, FOR NOW SIMPLY TAKE FIRST GENPARTICLE AS SUCH
     """
     for track in ev.Tracks ...
     """
 
-    hit_x[0] = 0.0
-    hit_y[0] = 0.0
-    hit_z[0] = 0.0
-    hit_t[0] = 0.0
-    hit_p[0] = part_p[0]
-    hit_e[0] = math.sqrt(part_p[0] ** 2 + part_m[0] ** 2)
-    hit_theta[0] = part_theta[0]
-    hit_phi[0] = part_phi[0]
-    hit_type[0] = 0  # 0 for tracks
-    hit_link1[0] = 0  # linked to first particle by default now
-    hit_link2[0] = -1
-    hit_link3[0] = -1
-    hit_link4[0] = -1
-    hit_link5[0] = -1
+    hit_x.push_back(0.0)
+    hit_y.push_back(0.0)
+    hit_z.push_back(0.0)
+    hit_t.push_back(0.0)
+    hit_p.push_back(part_p[0])
+    hit_e.push_back(math.sqrt(part_p[0] ** 2 + part_m[0] ** 2))
+    hit_theta.push_back(part_theta[0])
+    hit_phi.push_back(part_phi[0])
+    hit_type.push_back(0)  # 0 for tracks
+
+    link_vector = ROOT.std.vector("int")()
+    link_vector.push_back(0)
+    hit_genlink.push_back(link_vector) # linked to first particle by default now
 
     n_hit[0] += 1
 
     ## now loop ovoer ECAL hits
-    j = 0
     for hit in ev.ECalBarrelPositionedHits:
         if hit.energy < MIN_CALO_ENERGY:
             continue
 
-        index = n_hit[0] + j
         r = math.sqrt(hit.position.x ** 2 + hit.position.y ** 2 + hit.position.z ** 2)
         theta = math.acos(hit.position.z / r)
         phi = math.atan2(hit.position.y, hit.position.x)
 
-        hit_x[index] = hit.position.x
-        hit_y[index] = hit.position.y
-        hit_z[index] = hit.position.z
-        hit_t[index] = 0.0
-        hit_p[index] = hit.energy
-        hit_e[index] = hit.energy
-        hit_theta[index] = theta
-        hit_phi[index] = phi
-        hit_type[index] = 1  # 1 for ECAL
-        hit_link1[index] = 0  # linked to first particle by default now
-        hit_link2[index] = -1
-        hit_link3[index] = -1
-        hit_link4[index] = -1
-        hit_link5[index] = -1
+        hit_x.push_back(hit.position.x)
+        hit_y.push_back(hit.position.y)
+        hit_z.push_back(hit.position.z)
+        hit_t.push_back(0.0)
+        hit_p.push_back(hit.energy)
+        hit_e.push_back(hit.energy)
+        hit_theta.push_back(theta)
+        hit_phi.push_back(phi)
+        hit_type.push_back(1)  # 1 for ECAL
+
+        link_vector = ROOT.std.vector("int")()
+        link_vector.push_back(0)
+        hit_genlink.push_back(link_vector) # linked to first particle by default now
 
         n_hit[0] += 1
-        j += 1
 
     ## now loop over HCAL hits
-    j = 0
     for hit in ev.HCalBarrelPositionedHits:
         if hit.energy < MIN_CALO_ENERGY:
             continue
 
-        index = n_hit[0] + j
-
         r = math.sqrt(hit.position.x ** 2 + hit.position.y ** 2 + hit.position.z ** 2)
         theta = math.acos(hit.position.z / r)
         phi = math.atan2(hit.position.y, hit.position.x)
 
-        hit_x[index] = hit.position.x
-        hit_y[index] = hit.position.y
-        hit_z[index] = hit.position.z
-        hit_t[index] = 0.0
-        hit_p[index] = hit.energy
-        hit_e[index] = hit.energy
-        hit_theta[index] = theta
-        hit_phi[index] = phi
-        hit_type[index] = 2  # 2 for HCAL
-        hit_link1[index] = 0  # linked to first particle by default now
-        hit_link2[index] = -1
-        hit_link3[index] = -1
-        hit_link4[index] = -1
-        hit_link5[index] = -1
+        hit_x.push_back(hit.position.x)
+        hit_y.push_back(hit.position.y)
+        hit_z.push_back(hit.position.z)
+        hit_t.push_back(0.0)
+        hit_p.push_back(hit.energy)
+        hit_e.push_back(hit.energy)
+        hit_theta.push_back(theta)
+        hit_phi.push_back(phi)
+        hit_type.push_back(2) # 2 for HCAL
+
+        link_vector = ROOT.std.vector("int")()
+        link_vector.push_back(0)
+        hit_genlink.push_back(link_vector) # linked to first particle by default now
 
         n_hit[0] += 1
-        j += 1
 
     print(i, n_hit[0], n_part[0])
     t.Fill()
