58f29930a014ed8b091046e69c5ee56cb085d065
doloresgarcia <doloresg382@gmail.com>
2023-11-02 19:58:43 +0100

    db

--- src/layers/inference_oc.py
+++ src/layers/inference_oc.py
@@ -112,7 +112,11 @@ def generate_showers_data_frame(
     energy_t = dic["part_true"][:, 3]
     index_matches = col_ind + 1
     index_matches = index_matches.to(energy_t.device)
-    matched_es = torch.zeros_like(energy_t).to(energy_t.device) * torch.nan
+    matched_es = torch.zeros_like(energy_t) * torch.nan
+    matched_es = matched_es.to(energy_t.device)
+    print(
+        matched_es.device, row_ind.device, e_pred_showers.device, index_matches.device
+    )
     matched_es[row_ind] = e_pred_showers[index_matches]
     intersection_E = torch.zeros_like(energy_t) * torch.nan
     ie_e = obtain_intersection_values(i_m_w, row_ind, col_ind)
