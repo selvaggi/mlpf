c66a5f3f46240244093f58dc7aeb92ca2295753f
doloresgarcia <doloresg382@gmail.com>
2023-11-21 17:00:02 +0100

    db

--- src/dataset/functions_graph.py
+++ src/dataset/functions_graph.py
@@ -65,7 +65,8 @@ def find_mask_no_energy(hit_particle_link, hit_type_a, hit_energies, y):
             or (number_of_hits[index] < 20)
         ):  # This is commented to disable filtering
             list_remove.append(p)
-            assert part_frac[int(p) - 1] <= energy_cut
+            print("percentage of energy, number of hits" part_frac[int(p) - 1],number_of_hits[index], y[index,3])
+            #assert part_frac[int(p) - 1] <= energy_cut
 
     if len(list_remove) > 0:
         mask = torch.tensor(np.full((len(hit_particle_link)), False, dtype=bool))
