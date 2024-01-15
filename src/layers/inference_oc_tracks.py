13545bbc02b1ebe10ecb50cdc458fa3fd800fc22
doloresgarcia <doloresg382@gmail.com>
2024-01-15 20:52:48 +0100

    db

--- src/layers/inference_oc_tracks.py
+++ src/layers/inference_oc_tracks.py
@@ -272,6 +272,8 @@ def get_clustering(betas: torch.Tensor, X: torch.Tensor, tbeta=0.1, td=0.5):
 
 
 def obtain_intersection_values(intersection_matrix_w, row_ind, col_ind):
+    print("intersection_matrix_w", intersection_matrix_w.shape)
+    print(col_ind.shape)
     list_intersection_E = []
     # intersection_matrix_w = intersection_matrix_w
     intersection_matrix_wt = torch.transpose(intersection_matrix_w[1:, :], 1, 0)
