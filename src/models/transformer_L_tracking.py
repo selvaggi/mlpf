2e86d4b00b902a8473fd867e71a497990e686514
doloresgarcia <doloresg382@gmail.com>
2024-02-06 16:12:36 +0100

    graph transformer for tracking

---
+++ src/models/transformer_L_tracking.py
@@ -0,0 +1,385 @@
+import torch
+import torch.nn as nn
+from torch import Tensor
+from torch_scatter import scatter_min, scatter_max, scatter_mean, scatter_add
+from torch.nn import Parameter
+from src.layers.GravNetConv3 import GravNetConv, WeirdBatchNorm
+import numpy as np
+from typing import Tuple, Union, List
+import dgl
+from src.logger.plotting_tools import PlotCoordinates
+from src.layers.obj_cond_inf import calc_energy_loss
+from src.models.gravnet_calibration import (
+    obtain_batch_numbers,
+)
+from src.layers.GravNetConv3 import knn_per_graph
+from src.layers.inference_oc import create_and_store_graph_output
+import lightning as L
+from src.utils.nn.tools import log_losses_wandb_tracking
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+from src.layers.inference_oc_tracks import evaluate_efficiency_tracks
+from src.layers.object_cond import calc_LV_Lbeta
+from src.layers.graph_transformer_layer import GraphTransformerLayer
+from src.layers.mlp_readout_layer import MLPReadout
+
+
+class GraphT(L.LightningModule):
+    def __init__(
+        self,
+        args,
+        dev,
+        input_dim: int = 5,
+        output_dim: int = 4,
+        n_postgn_dense_blocks: int = 3,
+        n_gravnet_blocks: int = 4,
+        clust_space_norm: str = "twonorm",
+        k_gravnet: int = 7,
+        activation: str = "elu",
+        weird_batchnom=False,
+    ):
+
+        super(GraphT, self).__init__()
+        self.loss_final = 100
+        self.df_showers = []
+        self.df_showers_pandora = []
+        self.df_showes_db = []
+        self.args = args
+        self.validation_step_outputs = []
+        in_dim_node = 5  # node_dim (feat is an integer)
+        hidden_dim = 80  # before 80
+        out_dim = 80
+        n_classes = 4
+        num_heads = 8
+        in_feat_dropout = 0.0
+        dropout = 0.0
+        n_layers = 10
+        self.n_layers = n_layers
+        self.layer_norm = False
+        self.batch_norm = False
+        self.residual = True
+        self.dropout = dropout
+        self.n_classes = n_classes
+        self.lap_pos_enc = False
+        self.wl_pos_enc = False
+        max_wl_role_index = 100
+        self.readout = "sum"
+        self.output_dim = n_classes
+        self.batchnorm1 = nn.BatchNorm1d(in_dim_node, momentum=0.01)
+        self.embedding_h = nn.Linear(in_dim_node, hidden_dim)  # node feat is an integer
+        # self.embedding_h.weight.data.copy_(torch.eye(hidden_dim, in_dim_node))
+        self.in_feat_dropout = nn.Dropout(in_feat_dropout)
+        self.layers = nn.ModuleList(
+            [
+                GraphTransformerLayer(
+                    hidden_dim,
+                    hidden_dim,
+                    num_heads,
+                    dropout,
+                    self.layer_norm,
+                    self.batch_norm,
+                    self.residual,
+                )
+                for _ in range(n_layers - 1)
+            ]
+        )
+        self.layers.append(
+            GraphTransformerLayer(
+                hidden_dim,
+                out_dim,
+                num_heads,
+                dropout,
+                self.layer_norm,
+                self.batch_norm,
+                self.residual,
+            )
+        )
+        self.MLP_layer = MLPReadout(out_dim, 64)
+        self.clustering = nn.Linear(64, 4 - 1, bias=False)
+        self.beta = nn.Linear(64, 1)
+
+    def forward(self, g, step_count):
+        original_coords = g.ndata["pos_hits_xyz"]
+        g.ndata["original_coords"] = original_coords
+        if step_count % 100 == 0:
+            PlotCoordinates(g, path="input_coords", outdir=self.args.model_prefix)
+        ############################## Embeddings #############################################
+        h = g.ndata["h"]
+        # input embedding
+        h = self.batchnorm1(h)
+        h = self.embedding_h(h)
+        h = self.in_feat_dropout(h)
+
+        # GraphTransformer Layers
+        gu = knn_per_graph(g, original_coords, 16)
+        gu.ndata["h"] = h
+        for conv in self.layers:
+            h = conv(gu, h)
+        x = self.MLP_layer(h)
+        x_cluster_coord = self.clustering(x)
+        beta = self.beta(x)
+        g.ndata["final_cluster"] = x_cluster_coord
+        g.ndata["beta"] = beta.view(-1)
+        if step_count % 100 == 0:
+            PlotCoordinates(
+                g,
+                path="final_clustering",
+                outdir=self.args.model_prefix,
+                predict=self.args.predict,
+            )
+        x = torch.cat((x_cluster_coord, beta.view(-1, 1)), dim=1)
+        return x
+
+    # def on_after_backward(self):
+    #     for name, p in self.named_parameters():
+    #         if p.grad is None:
+    #             print(name)
+
+    def training_step(self, batch, batch_idx):
+        y = batch[1]
+
+        batch_g = batch[0]
+        if self.trainer.is_global_zero:
+            model_output = self(batch_g, batch_idx)
+        else:
+            model_output = self(batch_g, 1)
+
+        (loss, losses) = object_condensation_loss_tracking(
+            batch_g,
+            model_output,
+            y,
+            clust_loss_only=True,
+            add_energy_loss=False,
+            calc_e_frac_loss=False,
+            q_min=self.args.qmin,
+            frac_clustering_loss=self.args.frac_cluster_loss,
+            attr_weight=self.args.L_attractive_weight,
+            repul_weight=self.args.L_repulsive_weight,
+            fill_loss_weight=self.args.fill_loss_weight,
+            use_average_cc_pos=self.args.use_average_cc_pos,
+            hgcalloss=self.args.hgcalloss,
+            tracking=True,
+        )
+        loss = loss
+
+        if self.trainer.is_global_zero:
+            log_losses_wandb_tracking(True, batch_idx, 0, losses, loss)
+
+        self.loss_final = loss
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        self.validation_step_outputs = []
+        y = batch[1]
+
+        batch_g = batch[0]
+
+        model_output = self(batch_g, 1)
+        preds = model_output.squeeze()
+
+        (loss, losses) = object_condensation_loss_tracking(
+            batch_g,
+            model_output,
+            y,
+            q_min=self.args.qmin,
+            frac_clustering_loss=0,
+            clust_loss_only=self.args.clustering_loss_only,
+            use_average_cc_pos=self.args.use_average_cc_pos,
+            hgcalloss=self.args.hgcalloss,
+            tracking=True,
+        )
+        loss = loss  # + 0.01 * loss_ll  # + 1 / 20 * loss_E  # add energy loss # loss +
+        if self.trainer.is_global_zero:
+            log_losses_wandb_tracking(True, batch_idx, 0, losses, loss, val=True)
+        self.validation_step_outputs.append([model_output, batch_g, y])
+        if self.trainer.is_global_zero:
+            evaluate_efficiency_tracks(
+                batch_g,
+                model_output,
+                y,
+                0,
+                batch_idx,
+                0,
+                path_save=self.args.model_prefix + "showers_df_evaluation",
+                store=True,
+                predict=True,
+            )
+
+    def on_train_epoch_end(self):
+
+        # log epoch metric
+        self.log("train_loss_epoch", self.loss_final)
+
+    def on_train_epoch_start(self):
+        self.make_mom_zero()
+
+    def on_validation_epoch_start(self):
+        self.make_mom_zero()
+        self.df_showers = []
+        self.df_showers_pandora = []
+        self.df_showes_db = []
+
+    def make_mom_zero(self):
+        if self.current_epoch > 2 or self.args.predict:
+            self.ScaledGooeyBatchNorm2_1.momentum = 0
+            self.ScaledGooeyBatchNorm2_2.momentum = 0
+            for num_layer, gravnet_block in enumerate(self.gravnet_blocks):
+                gravnet_block.batchnorm_gravnet1.momentum = 0
+                gravnet_block.batchnorm_gravnet2.momentum = 0
+
+    # def on_validation_epoch_end(self):
+    #     if self.trainer.is_global_zero:
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        return {
+            "optimizer": optimizer,
+            "lr_scheduler": {
+                "scheduler": ReduceLROnPlateau(optimizer),
+                "interval": "epoch",
+                "monitor": "train_loss_epoch",
+                "frequency": 1
+                # If "monitor" references validation metrics, then "frequency" should be set to a
+                # multiple of "trainer.check_val_every_n_epoch".
+            },
+        }
+
+
+class GravNetBlock(nn.Module):
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int = 96,
+        space_dimensions: int = 3,
+        propagate_dimensions: int = 22,
+        k: int = 40,
+        # batchnorm: bool = True
+        weird_batchnom=False,
+    ):
+        super(GravNetBlock, self).__init__()
+        self.d_shape = 64
+        out_channels = self.d_shape
+        propagate_dimensions = self.d_shape
+        self.gravnet_layer = GravNetConv(
+            self.d_shape,
+            out_channels,
+            space_dimensions,
+            propagate_dimensions,
+            k,
+            weird_batchnom,
+        ).jittable()
+
+        self.post_gravnet = nn.Sequential(
+            nn.Linear(
+                out_channels + space_dimensions + self.d_shape, self.d_shape
+            ),  #! Dense 3
+            nn.ELU(),
+            nn.Linear(self.d_shape, self.d_shape),  #! Dense 4
+            nn.ELU(),
+        )
+        self.pre_gravnet = nn.Sequential(
+            nn.Linear(in_channels, self.d_shape),  #! Dense 1
+            nn.ELU(),
+            nn.Linear(self.d_shape, self.d_shape),  #! Dense 2
+            nn.ELU(),
+        )
+
+    def forward(
+        self,
+        g,
+        x: Tensor,
+        batch: Tensor,
+        original_coords: Tensor,
+        step_count,
+        outdir,
+        num_layer,
+    ) -> Tensor:
+        x = self.pre_gravnet(x)
+        x_input = x
+        xgn, graph, gncoords, loss_regularizing_neig, ll_r = self.gravnet_layer(
+            g, x, original_coords, batch
+        )
+        g.ndata["gncoords"] = gncoords
+        # if step_count % 50:
+        #     PlotCoordinates(
+        #         g, path="gravnet_coord", outdir=outdir, num_layer=str(num_layer)
+        #     )
+        # gncoords = gncoords.detach()
+        x = torch.cat((xgn, gncoords, x_input), dim=1)
+        x = self.post_gravnet(x)
+        return x, graph, loss_regularizing_neig, ll_r
+
+
+def init_weights(m):
+    if isinstance(m, nn.Linear):
+        torch.nn.init.xavier_uniform(m.weight)
+        m.bias.data.fill_(0.00)
+
+
+def object_condensation_loss_tracking(
+    batch,
+    pred,
+    y,
+    return_resolution=False,
+    clust_loss_only=True,
+    add_energy_loss=False,
+    calc_e_frac_loss=False,
+    q_min=0.1,
+    frac_clustering_loss=0.1,
+    attr_weight=1.0,
+    repul_weight=1.0,
+    fill_loss_weight=1.0,
+    use_average_cc_pos=0.0,
+    hgcalloss=False,
+    output_dim=4,
+    clust_space_norm="none",
+    tracking=False,
+):
+
+    _, S = pred.shape
+    if clust_loss_only:
+        clust_space_dim = output_dim - 1
+    else:
+        clust_space_dim = output_dim - 28
+
+    bj = torch.sigmoid(torch.reshape(pred[:, clust_space_dim], [-1, 1]))  # 3: betas
+    original_coords = batch.ndata["h"][:, 0:clust_space_dim]
+    xj = pred[:, 0:clust_space_dim]  # xj: cluster space coords
+
+    dev = batch.device
+    clustering_index_l = batch.ndata["particle_number"]
+
+    len_batch = len(batch.batch_num_nodes())
+    batch_numbers = torch.repeat_interleave(
+        torch.range(0, len_batch - 1).to(dev), batch.batch_num_nodes()
+    ).to(dev)
+
+    a = calc_LV_Lbeta(
+        original_coords,
+        batch,
+        y,
+        None,
+        None,
+        momentum=None,
+        predicted_pid=None,
+        beta=bj.view(-1),
+        cluster_space_coords=xj,  # Predicted by model
+        cluster_index_per_event=clustering_index_l.view(
+            -1
+        ).long(),  # Truth hit->cluster index
+        batch=batch_numbers.long(),
+        qmin=q_min,
+        return_regression_resolution=return_resolution,
+        post_pid_pool_module=None,
+        clust_space_dim=clust_space_dim,
+        frac_combinations=frac_clustering_loss,
+        attr_weight=attr_weight,
+        repul_weight=repul_weight,
+        fill_loss_weight=fill_loss_weight,
+        use_average_cc_pos=use_average_cc_pos,
+        hgcal_implementation=hgcalloss,
+        tracking=tracking,
+    )
+
+    loss = a[0] + a[1]  # + 5 * a[14]
+
+    return loss, a
---
+++ src/models/wrapper/example_graphT_model_tracking.py
@@ -0,0 +1,40 @@
+import torch
+
+from src.models.transformer_L_tracking import GraphT
+
+# from src.models.GATr.Gatr import ExampleWrapper as GravnetModel
+
+
+class GraphTransformerNetWrapper(torch.nn.Module):
+    def __init__(self, args, dev, **kwargs) -> None:
+        super().__init__()
+        self.mod = GraphT(args, dev, **kwargs)
+
+    def forward(self, g, step_count):
+        return self.mod(g, step_count)
+
+
+def get_model(data_config, args, dev, **kwargs):
+    # pf_features_dims = len(data_config.input_dicts['pf_features'])
+    # num_classes = len(data_config.label_value)
+    print("Model options: ", kwargs)
+    model = GraphTransformerNetWrapper(args, dev, **kwargs)
+
+    model_info = {
+        "input_names": list(data_config.input_names),
+        "input_shapes": {
+            k: ((1,) + s[1:]) for k, s in data_config.input_shapes.items()
+        },
+        "output_names": ["softmax"],
+        "dynamic_axes": {
+            **{k: {0: "N", 2: "n_" + k.split("_")[0]} for k in data_config.input_names},
+            **{"softmax": {0: "N"}},
+        },
+    }
+
+    return model, model_info
+
+
+def get_loss(data_config, **kwargs):
+
+    return torch.nn.MSELoss()
