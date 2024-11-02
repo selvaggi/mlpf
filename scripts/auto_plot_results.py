538341ce315756c94f9f82b9672414a541267b63
Gregor Krzmanc <gregor.krzmanc@cern.ch>
2024-11-02 11:16:56 +0100

    add auto_plot_results.py script

--- scripts/auto_plot_results.py
+++ scripts/auto_plot_results.py
@@ -16,9 +16,12 @@ gpu = int(args.gpu)
 dir_ckpt = os.path.join(args.path, "eval_results")
 if not os.path.exists(dir_ckpt):
     os.makedirs(dir_ckpt)
-
+import os
+# PYTHONOPATH=$PYTHONPATH:/afs/cern.ch/work/g/gkrzmanc/mlpf_2024
+# quick fix for path problems
+os.environ["PYTHONPATH"] = os.environ.get("PYTHONPATH", "") + ":/afs/cern.ch/work/g/gkrzmanc/mlpf_2024"
 eval_cmd = """/home/gkrzmanc/gatr/bin/python -m src.train_lightning1 --data-test /eos/experiment/fcc/ee/datasets/mlpf/CLD/train/011024_Hcard_eval/pf_tree_4002.root /eos/experiment/fcc/ee/datasets/mlpf/CLD/train/011024_Hcard_eval/pf_tree_4003.root /eos/experiment/fcc/ee/datasets/mlpf/CLD/train/011024_Hcard_eval/pf_tree_4004.root /eos/experiment/fcc/ee/datasets/mlpf/CLD/train/011024_Hcard_eval/pf_tree_4005.root /eos/experiment/fcc/ee/datasets/mlpf/CLD/train/011024_Hcard_eval/pf_tree_4006.root /eos/experiment/fcc/ee/datasets/mlpf/CLD/train/011024_Hcard_eval/pf_tree_4007.root /eos/experiment/fcc/ee/datasets/mlpf/CLD/train/011024_Hcard_eval/pf_tree_4008.root /eos/experiment/fcc/ee/datasets/mlpf/CLD/train/011024_Hcard_eval/pf_tree_4009.root /eos/experiment/fcc/ee/datasets/mlpf/CLD/train/011024_Hcard_eval/pf_tree_4010.root /eos/experiment/fcc/ee/datasets/mlpf/CLD/train/011024_Hcard_eval/pf_tree_4011.root /eos/experiment/fcc/ee/datasets/mlpf/CLD/train/011024_Hcard_eval/pf_tree_4012.root /eos/experiment/fcc/ee/datasets/mlpf/CLD/train/011024_Hcard_eval/pf_tree_4013.root /eos/experiment/fcc/ee/datasets/mlpf/CLD/train/011024_Hcard_eval/pf_tree_4014.root /eos/experiment/fcc/ee/datasets/mlpf/CLD/train/011024_Hcard_eval/pf_tree_4015.root /eos/experiment/fcc/ee/datasets/mlpf/CLD/train/011024_Hcard_eval/pf_tree_4016.root /eos/experiment/fcc/ee/datasets/mlpf/CLD/train/011024_Hcard_eval/pf_tree_4017.root /eos/experiment/fcc/ee/datasets/mlpf/CLD/train/011024_Hcard_eval/pf_tree_4018.root /eos/experiment/fcc/ee/datasets/mlpf/CLD/train/011024_Hcard_eval/pf_tree_4019.root /eos/experiment/fcc/ee/datasets/mlpf/CLD/train/011024_Hcard_eval/pf_tree_4020.root --data-config config_files/config_hits_track_v1.yaml -clust -clust_dim 3 --network-config src/models/wrapper/example_mode_gatr_e.py --model-prefix {model_prefix} --wandb-displayname evalHss_reprod_clust_only_newmodel_Clustering2810 --num-workers 0 --gpus {gpu} --batch-size 8 --start-lr 1e-3 --num-epochs 100 --optimizer ranger --fetch-step 0.1 --condensation --log-wandb --wandb-projectname mlpf_debug_eval --wandb-entity fcc_ml --frac_cluster_loss 0 --qmin 1 --use-average-cc-pos 0.99 --lr-scheduler reduceplateau --tracks --correction --ec-model gatr-neutrals --regress-pos --add-track-chis --load-model-weights  {ckpt_file}  --freeze-clustering --predict --regress-unit-p --PID-4-class"""
-
+plotting_cmd = """ /home/gkrzmanc/gatr/bin/python src/evaluation/evaluate_mass_Hss.py  --path {path} """
 while True:
     files = os.listdir(args.path)
     files = [f for f in files if f.endswith(".ckpt")]
@@ -34,18 +37,46 @@ while True:
         current_folder_path = os.path.join(dir_ckpt, folder_name)
         if os.path.exists(current_folder_path):
             print(f"Skipping {f}, already evaluated")
-            continue
-        print(f"Running evaluation for {ckpt_file}")
-        cmd = eval_cmd.format(model_prefix=current_folder_path, gpu=gpu, ckpt_file=ckpt_file)
-        cmdargs = cmd.split()
-        proc = subprocess.Popen(cmdargs, stdout=subprocess.PIPE, shell=False)
-        (out, err) = proc.communicate()
-        # save stdout and stderr to files
-        with open(os.path.join(current_folder_path, "stdout.txt"), "w") as f:
-            f.write(out.decode())
-        with open(os.path.join(current_folder_path, "stderr.txt"), "w") as f:
-            f.write(err.decode())
-        print(f"Finished evaluation for {ckpt_file}")
+        else:
+            print(f"Running evaluation for {ckpt_file}")
+            cmd = eval_cmd.format(model_prefix=current_folder_path, gpu=gpu, ckpt_file=ckpt_file)
+            cmdargs = cmd.split()
+            proc = subprocess.Popen(cmdargs, stdout=subprocess.PIPE, shell=False)
+            (out, err) = proc.communicate()
+            # save stdout and stderr to files
+            with open(os.path.join(current_folder_path, "stdout.txt"), "w") as f:
+                if out is not None:
+                    f.write(out.decode())
+                else:
+                    f.write("None")
+            with open(os.path.join(current_folder_path, "stderr.txt"), "w") as f:
+                if err is not None:
+                    f.write(err.decode())
+                else:
+                    f.write("None")
+            print(f"Finished evaluation for {ckpt_file}")
+        plots_stdout = os.path.join(current_folder_path, "plots_stdout.txt")
+        plots_stderr = os.path.join(current_folder_path, "plots_stderr.txt")
+        # if plots stdout file exitss
+        if os.path.exists(plots_stdout):
+            print(f"Plots already exist for {f}")
+        else:
+            print("Plotting for ", current_folder_path)
+            cmd = plotting_cmd.format(path=current_folder_path)
+            cmdargs = cmd.split()
+            proc = subprocess.Popen(cmdargs, stdout=subprocess.PIPE, shell=False)
+            (out, err) = proc.communicate()
+            # save stdout and stderr to files
+            with open(plots_stdout, "w") as f:
+                if out is not None:
+                    f.write(out.decode())
+                else:
+                    f.write("None")
+            with open(plots_stderr, "w") as f:
+                if err is not None:
+                    f.write(err.decode())
+                else:
+                    f.write("None")
     time.sleep(pause)
     if time() - os.path.getmtime(os.path.join(args.path, files[-1])) > timeout:
         print("No new files found in the last 8 hrs, stopping")
