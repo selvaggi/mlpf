f9e6acbd31ccffd2140a7b31daf002737d3492bb
doloresgarcia <doloresg382@gmail.com>
2024-11-04 10:16:39 +0100

    gatr 12 blocks

--- src/train_lightning1.py
+++ src/train_lightning1.py
@@ -193,13 +193,13 @@ def main():
     if args.data_test:
         if args.load_model_weights is not None and args.correction:
             print("TODO: change imported the model for testing manually")
-            from src.models.GATr.Gatr_pf_e import ExampleWrapper as GravnetModel
+            from src.models.GATr.Gatr_pf_e_v2 import ExampleWrapper as GravnetModel
             model = GravnetModel.load_from_checkpoint(
                 args.load_model_weights, args=args, dev=0, map_location=dev, strict=False
             )
             print("weight before checkpoint", model.ec_model_wrapper_neutral.gatr.linear_out.s2mvs.weight)
             print("weight before checkpoint gatr", model.gatr.linear_out.s2mvs.weight)
-            model2 = model.load_from_checkpoint("/eos/user/m/mgarciam/datasets_mlpf/models_trained_CLD/061024_cont2/_epoch=0_step=5500.ckpt", args=args, dev=0,strict=False)
+            model2 = model.load_from_checkpoint("/eos/user/m/mgarciam/datasets_mlpf/models_trained_CLD/011124_Hss_v2/_epoch=4_step=37500.ckpt", args=args, dev=0,strict=False)
             #model2 = model.load_from_checkpoint("/eos/user/m/mgarciam/datasets_mlpf/models_trained_CLD/181024_Hss/_epoch=3_step=41000.ckpt", args=args, dev=0,strict=False)
             model.gatr = model2.gatr
             model.ScaledGooeyBatchNorm2_1 = model2.ScaledGooeyBatchNorm2_1
