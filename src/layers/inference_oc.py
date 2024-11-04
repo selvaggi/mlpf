136e548adc21c60c970bf5397d44a6289ba6a19e
doloresgarcia <doloresg382@gmail.com>
2024-11-04 17:26:55 +0100

    db

--- src/layers/inference_oc.py
+++ src/layers/inference_oc.py
@@ -514,7 +514,7 @@ def generate_showers_data_frame(
     matched_es_tracks = torch.zeros_like(energy_t) * (torch.nan)
     matched_es_tracks_1 = torch.zeros_like(energy_t) * (torch.nan)
     matched_es_tracks[row_ind_]=row_ind_.float()
-    matched_es_tracks_1[row_ind_]=tracks_label[index_matches]
+    matched_es_tracks_1[row_ind_]=tracks_label[index_matches].float()
     matched_es_tracks_1 = 1.0*(matched_es_tracks==matched_es_tracks_1)
     matched_es_tracks_1 = matched_es_tracks_1*is_track
 
