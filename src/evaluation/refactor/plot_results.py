0df2c1fe5144897b7f22dd50cba3821187616baf
Gregor Krzmanc <gregor.krzmanc@cern.ch>
2024-11-15 15:17:14 +0100

    add ablation studies, --mass-only plotting

--- src/evaluation/refactor/plot_results.py
+++ src/evaluation/refactor/plot_results.py
@@ -11,7 +11,8 @@ import os
 from src.utils.inference.pandas_helpers import open_hgcal, open_mlpf_dataframe
 from src.utils.inference.per_particle_metrics import (
     plot_per_energy_resolution2_multiple, plot_confusion_matrix, plot_confusion_matrix_pandora,
-    plot_efficiency_all, calc_unit_circle_dist, plot_per_energy_resolution2, analyze_fakes, plot_cm_per_energy, plot_fake_and_missed_energy_regions
+    plot_efficiency_all, calc_unit_circle_dist, plot_per_energy_resolution2, analyze_fakes,
+    plot_cm_per_energy, plot_fake_and_missed_energy_regions, quick_plot_mass
 )
 from src.utils.inference.track_cluster_eff_plots import plot_track_assignation_eval
 from src.utils.inference.event_Ks import get_decay_type
@@ -33,16 +34,18 @@ parser.add_argument("--preprocess", type=str, help="Comma-separated list of scri
                     default="")
 parser.add_argument("--output_dir", type=str, default="",
                     help="Output directory (just the name of the folder, nested under the input path")
-
+parser.add_argument("--mass-only", action="store_true", help="Only quickly plot mass in the energy resolution plots")
 args = parser.parse_args()
+
 print("Preprocess:", args.preprocess)
 PATH_store = os.path.join(args.path, args.output_dir)
-
+if not os.path.exists(PATH_store):
+    os.makedirs(PATH_store)
 import sys
 class Logger(object):
-    def __init__(self):
+    def __init__(self, filename='log.txt'):
         self.terminal = sys.stdout
-        self.log = open(os.path.join(PATH_store,"log.txt"), "a")
+        self.log = open(os.path.join(PATH_store, filename), "a")
 
     def write(self, message):
         self.terminal.write(message)
@@ -54,9 +57,8 @@ class Logger(object):
         # you might want to specify some extra behavior here.
         pass
 sys.stdout = Logger()
+sys.stderr = Logger("err.txt")
 
-if not os.path.exists(PATH_store):
-    os.makedirs(PATH_store)
 PATH_store_individual_plots = os.path.join(PATH_store, "individual_plots")
 PATH_store_detailed_plots = os.path.join(PATH_store, "summary_plots")
 if not os.path.exists(PATH_store_individual_plots):
@@ -68,27 +70,31 @@ path_pandora = "showers_df_evaluation/0_0_None_pandora.pt"
 dir_top = args.path
 print(PATH_store)
 path_hgcal = os.path.join(dir_top, path_ML)
-sd_hgb, matched_hgb = open_mlpf_dataframe(path_hgcal, False)
-sd_pandora, matched_pandora = open_mlpf_dataframe(os.path.join(dir_top, path_pandora), False)
-
-
+sd_hgb, _ = open_mlpf_dataframe(path_hgcal, False)
+sd_pandora, _ = open_mlpf_dataframe(os.path.join(dir_top, path_pandora), False)
 sd_hgb, sd_pandora = preprocess_dataframe(sd_hgb, sd_pandora, args.preprocess.split(","))
 
 
-plot_track_assignation_eval(sd_hgb, sd_pandora, PATH_store_detailed_plots)
+if args.mass_only:
+    quick_plot_mass(sd_hgb, sd_pandora, PATH_store_detailed_plots)
+    sys.exit(0)
+
+#plot_track_assignation_eval(sd_hgb, sd_pandora, PATH_store_detailed_plots)
 analyze_fakes(sd_pandora, sd_hgb, PATH_store_detailed_plots)
-pandora_vertex = np.array(sd_pandora.vertex.values.tolist())
 plot_fake_and_missed_energy_regions(sd_pandora, sd_hgb, PATH_store_detailed_plots)
-# drop nan values
-mask_nan = np.isnan(pandora_vertex).any(axis=1)
+
+pandora_vertex = np.array(sd_pandora.vertex.values.tolist())
+
+# Drop nan values
+'''mask_nan = np.isnan(pandora_vertex).any(axis=1)
 pandora_vertex = pandora_vertex[~mask_nan]
 hgb_vertex = np.array(sd_hgb.vertex.values.tolist())
 mask_nan_hgb = np.isnan(hgb_vertex).any(axis=1)
 hgb_vertex = hgb_vertex[~mask_nan_hgb]
 displacement_pandora = np.linalg.norm(pandora_vertex, axis=1)
-displacement_hgb = np.linalg.norm(hgb_vertex, axis=1)
-# filter the df based on where decay type is 0
-ranges = [[0, 5000]]   # Ranges of the displacement to make the plots from, in cm
+displacement_hgb = np.linalg.norm(hgb_vertex, axis=1)'''
+
+# Filter the df based on where decay type is 0
 fig, ax = plt.subplots(4, 5, figsize=(22, 22*4/5)) # The overview figure of efficiencies
 plot_cm_per_energy(sd_hgb, sd_pandora, PATH_store_detailed_plots, PATH_store_individual_plots)
 plot_efficiency_all(sd_pandora, [sd_hgb], PATH_store_individual_plots, ["ML"], ax=ax)
@@ -103,56 +109,38 @@ fig.add_artist(plt.Line2D([x_position, x_position], [0, 1], color="black", linew
 fig.tight_layout()
 fig.savefig(os.path.join(PATH_store_detailed_plots, "overview_Efficiency_FakeRate_ConfusionMatrix.pdf"))
 
-for range in ranges:
-    allowed_batch_idx = np.where((displacement_hgb < range[1]*10) & (displacement_hgb > range[0]*10))[0]
-    sd_hgb_filtered = sd_hgb[sd_hgb.number_batch.isin(allowed_batch_idx)]
-    allowed_batch_idx_pandora = np.where((displacement_pandora < range[1]*10) & (displacement_pandora > range[0]*10))[0]
-    sd_pandora_filtered = sd_pandora[sd_pandora.number_batch.isin(allowed_batch_idx_pandora)]
-    sd_pandora_filtered = renumber_batch_idx(sd_pandora_filtered)
-    sd_hgb_filtered = renumber_batch_idx(sd_hgb_filtered)
-    x = sd_hgb_filtered.pred_ref_pt_matched[sd_hgb_filtered.is_track_in_cluster==1].values
-    x = np.stack(x)
-    x = np.linalg.norm(x, axis=1)
-    #fig, ax = plt.subplots()
-    #bins = np.linspace(0, 0.25, 50)
-    #ax.hist(x, bins=bins)
-    #fig.savefig(os.path.join(PATH_store_individual_plots, "track_momentum_norm.pdf"))
-    idx_pick_reco = np.where(x > 0.15)[0]  # If the track is super far away, pick the reco energy instead of the track energy (weird bad track)
-    sd_hgb_filtered[sd_hgb_filtered.is_track_in_cluster==1].calibrated_E.iloc[idx_pick_reco] = sd_hgb_filtered[sd_hgb_filtered.is_track_in_cluster==1].pred_showers_E.iloc[idx_pick_reco]
-    print("Range", range, ": Finished collection of data and started plotting")
-    e_ranges = [[0, 5], [5, 15], [15, 50]]
-    for i in e_ranges:
-        print("Range: ", i,
-            " | Pandora: ",
-            len(
-                sd_pandora[
-                    (sd_pandora.pandora_calibrated_pfo > i[0]) & (sd_pandora.pandora_calibrated_pfo < i[1])
-                ]
-            ),
-            "ML: ",
-            len(sd_hgb[(sd_hgb.calibrated_E > i[0]) & (sd_hgb.calibrated_E < i[1])]),
-        )
-    if len(ranges) == 1:
-        current_dir = PATH_store_individual_plots
-        current_dir_detailed = PATH_store_detailed_plots
-    else:
-        current_dir =  os.path.join(PATH_store_individual_plots, "plots_range_" + str(range[0]) + "_" + str(range[1]))
-        current_dir_detailed = os.path.join(PATH_store_detailed_plots, "plots_range_" + str(range[0]) + "_" + str(range[1]))
-    if not os.path.exists(current_dir):
-        os.makedirs(current_dir)
-    if not os.path.exists(current_dir_detailed):
-        os.makedirs(current_dir_detailed)
-    plot_per_energy_resolution2_multiple(
-        sd_pandora_filtered,
-        {"ML": sd_hgb_filtered},
-        current_dir,
-        tracks=True,
-        perfect_pid=False,
-        mass_zero=False,
-        ML_pid=True,
-        PATH_store_detailed_plots=current_dir_detailed
-    )
-    print("Done plotting")
+x = sd_hgb.pred_ref_pt_matched[sd_hgb.is_track_in_cluster==1].values
+x = np.stack(x)
+x = np.linalg.norm(x, axis=1)
+
+#fig, ax = plt.subplots()
+#bins = np.linspace(0, 0.25, 50)
+#ax.hist(x, bins=bins)
+#fig.savefig(os.path.join(PATH_store_individual_plots, "track_momentum_norm.pdf"))
+idx_pick_reco = np.where(x > 0.15)[0]  # If the track is super far away, pick the reco energy instead of the track energy (weird bad track)
+sd_hgb[sd_hgb.is_track_in_cluster==1].calibrated_E.iloc[idx_pick_reco] = sd_hgb[sd_hgb.is_track_in_cluster==1].pred_showers_E.iloc[idx_pick_reco]
+e_ranges = [[0, 5], [5, 15], [15, 50]]
+
+current_dir = PATH_store_individual_plots
+current_dir_detailed = PATH_store_detailed_plots
+#else:
+#current_dir =  os.path.join(PATH_store_individual_plots, "plots_range_" + str(range[0]) + "_" + str(range[1]))
+#current_dir_detailed = os.path.join(PATH_store_detailed_plots, "plots_range_" + str(range[0]) + "_" + str(range[1]))
+if not os.path.exists(current_dir):
+    os.makedirs(current_dir)
+if not os.path.exists(current_dir_detailed):
+    os.makedirs(current_dir_detailed)
+plot_per_energy_resolution2_multiple(
+    sd_pandora,
+    {"ML": sd_pandora},
+    current_dir,
+    tracks=True,
+    perfect_pid=False,
+    mass_zero=False,
+    ML_pid=True,
+    PATH_store_detailed_plots=current_dir_detailed
+)
+print("Done plotting")
 
 def save_dict(di_, filename_):
     with open(filename_, "wb") as f:
--- src/evaluation/refactor/preprocess.py
+++ src/evaluation/refactor/preprocess.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from src.utils.pid_conversion import pid_conversion_dict
 
 def renumber_batch_idx(df):
     # batch_idx has missing numbers
@@ -37,15 +38,58 @@ def apply_beta_correction(sd_hgb):
     sd_hgb = sd_hgb[filter]
     return sd_hgb
 
+def remove_fakes(df):
+    return df[~pd.isna(df.pid)]
+
+
+def ablation_study(df, column):
+    # Replace all except the column in df with ground truth - to study only the effect of a single column on the results
+    is_pandora = "pandora_calibrated_pfo" in df.columns
+    assert column in ["E", "p", "pid"]
+    fakes = pd.isna(df.pid)
+    missed = pd.isna(df.pred_showers_E)
+    if column == "E":
+        if is_pandora:
+            #df.loc[(~fakes) & (~missed), "pandora_calibrated_pfo"] = df.loc[~fakes, "true_showers_E"]
+            df.loc[(~fakes) & (~missed), "pandora_calibrated_pos"] = df.loc[~fakes, "true_pos"]  # set pxyz to true
+            df.loc[(~fakes) & (~missed), "pandora_pid"] = df.loc[~fakes, "pid"].map(pid_conversion_dict)  # set pxyz to true
+        else:
+            #df.loc[(~fakes) & (~missed), "calibrated_E"] = df.loc[~fakes, "true_showers_E"] # set E to true
+            df.loc[(~fakes) & (~missed), "pred_pos_matched"] = df.loc[~fakes, "true_pos"]  # set pxyz to true
+            df.loc[(~fakes) & (~missed), "pred_pid_matched"] = df.loc[~fakes, "pid"].map(pid_conversion_dict)  # set pxyz to true]
+    elif column == "p":
+        if is_pandora:
+            df.loc[(~fakes) & (~missed), "pandora_calibrated_pfo"] = df.loc[~fakes, "true_showers_E"]
+            #df.loc[(~fakes) & (~missed), "pandora_calibrated_pos"] = df.loc[~fakes, "true_pos"]  # set pxyz to true
+            df.loc[(~fakes) & (~missed), "pandora_pid"] = df.loc[~fakes, "pid"].map(pid_conversion_dict)  # set pxyz to true
+        else:
+            df.loc[(~fakes) & (~missed), "calibrated_E"] = df.loc[~fakes, "true_showers_E"] # set E to true
+            #df.loc[(~fakes) & (~missed), "pred_pos_matched"] = df.loc[~fakes, "true_pos"]  # set pxyz to true
+            df.loc[(~fakes) & (~missed), "pred_pid_matched"] = df.loc[~fakes, "pid"].map(pid_conversion_dict)  # set pxyz to true]
+    elif column == "pid":
+        if is_pandora:
+            df.loc[(~fakes) & (~missed), "pandora_calibrated_pfo"] = df.loc[~fakes, "true_showers_E"]
+            df.loc[(~fakes) & (~missed), "pandora_calibrated_pos"] = df.loc[~fakes, "true_pos"]  # set pxyz to true
+            #df.loc[(~fakes) & (~missed), "pandora_pid"] = df.loc[~fakes, "pid"].map(pid_conversion_dict)  # set pxyz to true
+        else:
+            df.loc[(~fakes) & (~missed), "calibrated_E"] = df.loc[~fakes, "true_showers_E"] # set E to true
+            df.loc[(~fakes) & (~missed), "pred_pos_matched"] = df.loc[~fakes, "true_pos"]  # set pxyz to true
+            #df.loc[(~fakes) & (~missed), "pred_pid_matched"] = df.loc[~fakes, "pid"].map(pid_conversion_dict)  # set pxyz to true]
+    return df
 def preprocess_dataframe(sd_hgb, sd_pandora, names=""):
     # names: list of scripts to do on data
     # sd_hgb: pandas dataframe with the HGB data
     # sd_pandora: pandas dataframe with the Pandora data
+    sd_hgb = sd_hgb[sd_hgb.reco_showers_E != 0.0]
+    sd_pandora = sd_pandora[sd_pandora.reco_showers_E != 0.0]
     if "class_correction" in names:
         sd_hgb = apply_class_correction(sd_hgb)
     sd_hgb.loc[sd_hgb.pred_pid_matched == 3, "calibrated_E"] = sd_hgb.loc[
         sd_hgb.pred_pid_matched == 3, "pred_showers_E"] # correct photons
     if "beta_correction" in names:
         sd_hgb = apply_beta_correction(sd_hgb)
+    if "remove_fakes" in names:
+        sd_hgb = remove_fakes(sd_hgb)
+        sd_pandora = remove_fakes(sd_pandora)
     return renumber_batch_idx(sd_hgb), renumber_batch_idx(sd_pandora)
 
