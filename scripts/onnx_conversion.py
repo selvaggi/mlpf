fbbb0a8cca5b038a72146fde1209a1541c8a42db
herrmannlena <lena.maria.herrmann@cern.ch>
2025-07-15 15:32:11 +0200

    change onnx conversion method

--- scripts/onnx_conversion.py
+++ scripts/onnx_conversion.py
@@ -45,23 +45,50 @@ model = GravnetModel.load_from_checkpoint(
                 )
 model.eval()
 
-pos_hits_xyz = torch.randn(10, 3).to("cpu")      
-hit_type = torch.randint(0, 3, (10,)).to("cpu")       
-h_scalar = torch.randn(10, 5).to("cpu")               
+pos_hits_xyz = torch.randn(10, 3).to("cpu")           # 3D coordinates
+hit_type = torch.randint(0, 3, (10,)).to("cpu")        # random categorical types
+h_scalar = torch.randn(10, 5).to("cpu")                # last 2 columns will be used (e.g., e, p)
 dummy_attention_mask = torch.randn(10, 10).to("cpu") 
 
-inputs = (pos_hits_xyz, hit_type, h_scalar, dummy_attention_mask)
 
 
 export_options = torch.onnx.ExportOptions(dynamic_shapes=True)
 
+inputs = (
+    pos_hits_xyz,
+    hit_type,  
+    h_scalar,
+    dummy_attention_mask
+)
 
 
-onnx_program = torch.onnx.dynamo_export(
-            model, pos_hits_xyz, hit_type, h_scalar, dummy_attention_mask, export_options=export_options)
+
+#onnx_program = torch.onnx.dynamo_export(
+#    model, pos_hits_xyz, hit_type, h_scalar, dummy_attention_mask, export_options=torch.onnx.ExportOptions(dynamic_shapes=True)
+#)
+
+
+# Export
+torch.onnx.export(
+    model,
+    inputs,
+    filepath,
+    input_names=["pos_hits_xyz", "hit_type", "h_scalar", "attention_mask"],
+    output_names=["output"],
+    opset_version=17,  
+    do_constant_folding=True,
+    dynamic_axes={
+        "pos_hits_xyz": {0: "batch"},
+        "hit_type": {0: "batch"},
+        "h_scalar": {0: "batch"},
+        "attention_mask": {0: "batch", 1: "points"},
+        "output": {0: "batch"}
+    },
+    verbose=True
+)
+
 
 print("filepath")
 print(filepath)        
-onnx_program.save(filepath)
+#onnx_program.save(filepath)
 
-print("exporting to onnx")
