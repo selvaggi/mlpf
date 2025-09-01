e320b3f157dd016822d9b9473d3acc9a1256f29f
Giovanni Marchiori <giovanni.marchiori@cern.ch>
2025-09-01 23:57:37 +0200

    documentation

--- condor_ALLEGRO/script_create_dataset_train.sh
+++ condor_ALLEGRO/script_create_dataset_train.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+# execute it from the directory containing the script
+# to have python: source /cvmfs/sw-nightlies.hsf.org/key4hep/setup.sh
 
-python submit_jobs.py --mode train --config config_spread_031224_fair.gun --outdir /eos/experiment/fcc/users/g/gmarchio/ALLEGRO_o1_v03/mlpf/train/gun_dr_logE_v0_010725/  --condordir /eos/experiment/fcc/users/g/gmarchio/ALLEGRO_o1_v03/mlpf/condor/gun_dr_log_logE_v0_290125/  --njobs 1600 --nev 1000 --queue nextweek
+python submit_jobs.py --mode train --config config_spread_031224_fair.gun --outdir /eos/experiment/fcc/users/g/gmarchio/ALLEGRO_o1_v03/mlpf/train/gun_dr_logE_v0_010925/  --condordir /eos/experiment/fcc/users/g/gmarchio/ALLEGRO_o1_v03/mlpf/condor/gun_dr_logE_v0_010925/  --njobs 1600 --nev 1000 --queue nextweek
 # --queue tomorrow
 
