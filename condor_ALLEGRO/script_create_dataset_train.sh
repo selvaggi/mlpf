94d0b262d33d23a40ec9103c6251e2ca6d915c4a
Giovanni Marchiori <giovanni.marchiori@cern.ch>
2025-09-02 10:50:36 +0200

    add some testing files

--- condor_ALLEGRO/script_create_dataset_train.sh
+++ condor_ALLEGRO/script_create_dataset_train.sh
@@ -5,3 +5,5 @@
 python submit_jobs.py --mode train --config config_spread_031224_fair.gun --outdir /eos/experiment/fcc/users/g/gmarchio/ALLEGRO_o1_v03/mlpf/train/gun_dr_logE_v0_010925/  --condordir /eos/experiment/fcc/users/g/gmarchio/ALLEGRO_o1_v03/mlpf/condor/gun_dr_logE_v0_010925/  --njobs 1600 --nev 1000 --queue nextweek
 # --queue tomorrow
 
+# test, 1 job
+# python submit_jobs.py --mode train --config config_spread_031224_fair_test.gun --outdir /eos/experiment/fcc/users/g/gmarchio/ALLEGRO_o1_v03/mlpf/train/gun_dr_logE_v0_010925/  --condordir /eos/experiment/fcc/users/g/gmarchio/ALLEGRO_o1_v03/mlpf/condor/gun_dr_logE_v0_010925/  --njobs 1 --nev 10 --queue longlunch
